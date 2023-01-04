d9cb062838031d432b17be95b172285d05f32927
Josef Friedrich <josef@friedrich.rocks>
2023-01-04 11:32:44 +0100

    Add small libraries

--- boilerplate.lua
+++ boilerplate.lua
@@ -15,9 +15,221 @@
 --
 -- This work consists of the files boilerplate.lua, boilerplate.tex,
 -- and boilerplate.sty.
+---
+---Small library to surround strings with ANSI color codes.
+---
+---The upstream source is located at: [boilerplate.lua](https://github.com/Josef-Friedrich/tex-project-boilerplate/blob/main/boilerplate.lua)
+--
+---[SGR (Select Graphic Rendition) Parameters](https://en.wikipedia.org/wiki/ANSI_escape_code#SGR_(Select_Graphic_Rendition)_parameters)
+---
+---__attributes__
+---
+---| color      |code|
+---|------------|----|
+---| reset      |  0 |
+---| clear      |  0 |
+---| bright     |  1 |
+---| dim        |  2 |
+---| underscore |  4 |
+---| blink      |  5 |
+---| reverse    |  7 |
+---| hidden     |  8 |
+---
+---__foreground__
+---
+---| color      |code|
+---|------------|----|
+---| black      | 30 |
+---| red        | 31 |
+---| green      | 32 |
+---| yellow     | 33 |
+---| blue       | 34 |
+---| magenta    | 35 |
+---| cyan       | 36 |
+---| white      | 37 |
+---
+---__background__
+---
+---| color      |code|
+---|------------|----|
+---| onblack    | 40 |
+---| onred      | 41 |
+---| ongreen    | 42 |
+---| onyellow   | 43 |
+---| onblue     | 44 |
+---| onmagenta  | 45 |
+---| oncyan     | 46 |
+---| onwhite    | 47 |
+---
+---@alias ColorName `black'|'red'|'green'|'yellow'|'blue'|'magenta'|'cyan'|'white`
+---@alias ColorMode `bright'|'dim`
+local ansi_color = (function()
+
+  ---
+  ---@param code integer
+  ---
+  ---@return string
+  local function format_color_code(code)
+    return string.char(27) .. '[' .. tostring(code) .. 'm'    ---
+    ---@param text string
+    ---    ---
+  end
+
+  ---
+  ---@param color ColorName # A color name.
+  ---@param mode? ColorMode
+  ---@param background? boolean # Colorize the background not the text.
+  ---
+  ---@return string
+  local function get_color_code(color, mode, background)
+
+    local output = ''
+    local code
+
+    if mode == 'bright' then
+      output = format_color_code(1)
+    elseif mode == 'dim' then
+      output = format_color_code(2)
+    end
+
+    if not background then
+      if color == 'reset' then
+        code = 0
+      elseif color == 'red' then
+        code = 31
+      elseif color == 'green' then
+        code = 32
+      elseif color == 'yellow' then
+        code = 33
+      elseif color == 'blue' then
+        code = 34
+      elseif color == 'magenta' then
+        code = 35
+      elseif color == 'cyan' then
+        code = 36
+      else
+        code = 37
+      end
+    else
+      if color == 'black' then
+        code = 40
+      elseif color == 'red' then
+        code = 41
+      elseif color == 'green' then
+        code = 42
+      elseif color == 'yellow' then
+        code = 43
+      elseif color == 'blue' then
+        code = 44
+      elseif color == 'magenta' then
+        code = 45
+      elseif color == 'cyan' then
+        code = 46
+      elseif color == 'white' then
+        code = 47
+      else
+        code = 40
+      end
+    end
+    return output .. format_color_code(code)
+  end
+
+  ---
+  ---@param color ColorName # A color name.
+  ---@param mode? ColorMode
+  ---@param background? boolean # Colorize the background not the text.
+  ---
+  ---@return string
+  local function surround_text(text, color, mode, background)
+    return string.format('%s%s%s',
+      get_color_code(color, mode, background), text,
+      get_color_code('reset'))
+  end
+
+  return {
+    ---
+    ---@param text string
+    ---
+    ---@return string
+    red = function(text)
+      return surround_text(text, 'red')
+    end,
+
+    ---
+    ---@param text string
+    ---
+    ---@return string
+    green = function(text)
+      return surround_text(text, 'green')
+    end,
+
+    ---
+    ---@param text string
+    ---
+    ---@return string
+    white = function(text)
+      return surround_text(text, 'white')
+    end,
+    ---@return string
+    yellow = function(text)
+      return surround_text(text, 'yellow')
+    end,
+
+    ---
+    ---@param text string
+    ---
+    ---@return string
+    blue = function(text)
+      return surround_text(text, 'blue')
+    end,
+
+    ---
+    ---@param text string
+    ---
+    ---@return string
+    cyan = function(text)
+      return surround_text(text, 'cyan')
+    end,
+  }
+end)()
+
+---
+---Small logging library.
+---
+---The upstream source is located at: [boilerplate.lua](https://github.com/Josef-Friedrich/tex-project-boilerplate/blob/main/boilerplate.lua)
+--
+local log = (function()
+  local opts = { verbosity = 0 }
+
+  local function print_message(message, ...)
+    print(string.format(message, ...))
+  end
+
+  local function info(message, ...)
+    if opts.verbosity > 0 then
+      print_message(message, ...)
+    end
+  end
+
+  local function debug(message, ...)
+    if opts.verbosity > 1 then
+      print_message(message, ...)
+    end
+  end
+
+  local function verbose(message, ...)
+    if opts.verbosity > 2 then
+      print_message(message, ...)
+    end
+  end
+
+  return { opts = opts, info = info, debug = debug, verbose = verbose }
+end)()
+
+log.opts.verbosity = 3
 
 return {
-  boilerplate = function ()
+  boilerplate = function()
     return 'boilerplate'
-  end
+  end,
 }
